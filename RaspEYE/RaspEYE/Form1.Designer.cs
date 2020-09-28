b39b95de279905074114371ac2d7efcd5c0a1646
Vikos <50700185+Vikosi@users.noreply.github.com>
2020-09-28 11:06:40 +0700

    Initial commit

---
+++ RaspEYE/RaspEYE/Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace RaspEYE
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(134, 45);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Открыть";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(176, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(470, 408);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(12, 232);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(134, 45);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Принять";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(667, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(450, 408);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(12, 232);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox3.TabIndex = 5;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 72);
+            this.label1.Name = "label1";
+            this.label1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label1.Size = new System.Drawing.Size(153, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Параметр t для бинеризации";
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(12, 283);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(134, 45);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Откат";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 120);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 8;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(12, 186);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(118, 17);
+            this.radioButton1.TabIndex = 9;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Отрисовка зрачка";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(12, 209);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(128, 17);
+            this.radioButton2.TabIndex = 10;
+            this.radioButton2.Text = "Отрисовка радужки";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.MouseClick += new System.Windows.Forms.MouseEventHandler(this.radioButton2_MouseClick);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 114);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(134, 43);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "Бинеризация";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(30, 88);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 21);
+            this.textBox1.TabIndex = 12;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TextBox1_KeyPress);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Location = new System.Drawing.Point(176, 426);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(470, 408);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox4.TabIndex = 13;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1129, 429);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Лабораторная работа 2";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox4;
+    }
+}
+
---
+++ RaspEYE/RaspEYE/Form1.cs
@@ -0,0 +1,287 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+
+namespace RaspEYE
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Bitmap image;
+
+        int y = 0;
+        int x = 0;
+        int wid = 0;
+        Bitmap bmp2;
+        int widz = 0;
+
+        //открытие
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog nf = new OpenFileDialog();
+            nf.Filter = "Image Files(*.BMP;*.JPG;*.JPEG;*.GIF;*.PNG)|*.BMP;*.JPG;*.JPEG;*.GIF;*.PNG|Все файлы(*.*)|*.*";
+            if (nf.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    pictureBox1.Image = new Bitmap(nf.FileName);
+                    button2.Visible = true;
+                    
+                    image = new Bitmap(pictureBox1.Image);
+                    kk = 0;
+                    pictureBox2.Visible = false;
+                    radioButton1.Checked = true;
+                    if (bmp2 != null)
+                    { bmp2.Dispose(); }
+                }
+                catch
+                {
+                    MessageBox.Show("Невозможно открыть файл");
+                }
+            }
+            button2.Enabled = false;
+            button3.Enabled = false;
+
+
+        }
+       
+        int kk = 0;
+        //отрисовка на оригинале
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+            if (kk >= 2)
+            {
+                MessageBox.Show("Изображение уже обработано");
+            }
+            else
+            {
+                pictureBox3.Image = pictureBox1.Image;
+                if (wid == widz && radioButton2.Checked == true)
+                { MessageBox.Show("Вы не выбрали параметры для радужки глаза"); kk = 1; }
+                else { paint3(); }
+
+                if (wid != widz && radioButton2.Checked == true)
+                {
+                    kk = 2;
+                }
+                if (radioButton1.Checked==true)
+                {
+                    kk = 1;
+                    radioButton2.Checked = true;
+                    Bitmap bmp1 = pictureBox3.Image as Bitmap;
+                    wid *= 4;
+                    bmp2 = bmp1.Clone(new Rectangle(x - wid / 2, y - wid / 2, wid, wid), bmp1.PixelFormat);
+                    paint4();
+                    widz = wid;
+                }
+
+
+            }
+            button3.Enabled = true;
+
+
+        }
+
+        public void paint4()
+        {
+            var im1 = new Bitmap(pictureBox3.Image);
+
+            Graphics g = Graphics.FromImage(im1);
+
+            Bitmap bmp1 = new Bitmap(bmp2, wid, wid);       //подгоняем размер картинки под размер прямоугольника     
+            g.Clear(Color.White);
+            g.DrawImage(bmp1, x - wid / 2, y - wid / 2);      //рисуем картинку
+            g.DrawRectangle(Pens.White, new Rectangle(x - wid / 2, y - wid / 2, wid, wid)); //рисуем прямоугольник         
+            pictureBox4.Image = im1;
+
+        }
+
+        public void paint3()
+        {
+            var im1 = new Bitmap(pictureBox1.Image);
+
+            Graphics g = Graphics.FromImage(im1);
+
+            g.DrawEllipse(new Pen(Color.Red, 3), new Rectangle(x - wid / 2, y - wid / 2, wid, wid));
+            pictureBox1.Image = im1;
+
+        }
+
+        public void paint()
+        {
+            var im2 = new Bitmap(pictureBox2.Image);
+
+            Graphics g1 = Graphics.FromImage(im2);
+            g1.DrawLine(new Pen(Color.Red, 3), new Point(0, y), new Point(im2.Width, y));
+            g1.DrawLine(new Pen(Color.Red, 3), new Point(x, 0), new Point(x, im2.Height));
+            g1.DrawEllipse(new Pen(Color.Red, 3), new Rectangle(x - wid / 2, y - wid / 2, wid, wid));
+            pictureBox2.Image = im2;
+        }
+
+        //откат
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            kk--;
+            if (kk == 0)
+            {
+                radioButton1.Checked = true;
+                pictureBox1.Image = image;
+                button3.Enabled = false;
+            }
+            else if (kk == 1)
+            {
+                pictureBox1.Image = pictureBox3.Image;
+            }
+        }
+
+        // отрисовка на чб
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int t = Convert.ToInt16(textBox1.Text);
+                if (t>255)
+                { MessageBox.Show("Слишком большое значение бинеризации"); textBox1.Text = "150"; return; }
+                button2.Enabled = true;
+                pictureBox2.Visible = true;
+                var bmp = new Bitmap(image);            
+                if (radioButton1.Checked == false)
+                { bmp.Dispose(); bmp = new Bitmap(pictureBox4.Image); }
+
+                for (int i = 0; i < bmp.Width; i++)
+                    for (int j = 0; j < bmp.Height; j++)
+                    {
+                        int R = bmp.GetPixel(i, j).R;
+                        int G = bmp.GetPixel(i, j).G;
+                        int B = bmp.GetPixel(i, j).B;
+                        int Gray = Convert.ToInt32(0.2125 * R + 0.7154 * G + 0.0721 * B);
+                        if (Gray < t)
+                        {
+                            R = G = B = 0;
+                        }
+                        else
+                        {
+                            R = G = B = 255;
+                        }
+                        Color RGB = Color.FromArgb(255, R, G, B);
+                        bmp.SetPixel(i, j, RGB);
+                    }
+
+                pictureBox2.Image = bmp;
+
+                Bitmap b = new Bitmap(pictureBox2.Width, pictureBox2.Height);
+                b = (Bitmap)pictureBox2.Image;
+
+                int[] counts = new int[b.Height];
+                for (int i = 0; i < b.Height; i++)
+                {
+                    counts[i] = 0;
+                }
+                int maxxx = 0;
+                for (int i = 0; i < b.Height; i++)
+                {
+                    maxxx = 0;
+                    for (int j = 0; j < b.Width; j++)
+                    {
+                        int r = b.GetPixel(j, i).R;
+                        if (r == 0)
+                        { counts[i] += 1; maxxx++; }
+                    }
+
+                }
+
+                int max = counts[0];
+                int maxid = 0;
+
+                for (int i = 1; i < counts.Length; i++)
+                {
+                    if (counts[i] > max)
+                    {
+                        max = counts[i];
+                        maxid = i;
+                    }
+                }
+                wid = max;
+                if (radioButton1.Checked == true)
+                { y = maxid; }
+
+                //--------------------------------------------------------------------------
+
+                counts = new int[b.Width];
+                for (int i = 0; i < b.Width; i++)
+                {
+                    counts[i] = 0;
+                }
+                maxxx = 0;
+                for (int i = 0; i < b.Width; i++)
+                {
+                    maxxx = 0;
+                    for (int j = 0; j < b.Height; j++)
+                    {
+                        int r = b.GetPixel(i, j).R;
+                        if (r == 0)
+                        { counts[i] += 1; maxxx++; }
+                    }
+                }
+
+                max = counts[0];
+                maxid = 0;
+
+                for (int i = 1; i < counts.Length; i++)
+                {
+                    if (counts[i] > max)
+                    {
+                        max = counts[i];
+                        maxid = i;
+                    }
+                }
+                if (radioButton1.Checked == true)
+                { x = maxid; }
+                 
+               
+                paint();
+                
+            }
+            catch
+            {
+                MessageBox.Show("Не введен параметр t");
+            }
+        }
+        
+
+        private void radioButton2_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (kk == 0)
+            {
+                radioButton1.Checked = true;
+                MessageBox.Show("Не опознан зрачок глаза");
+
+            }
+        }
+
+        private void TextBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char number = e.KeyChar;
+
+            if (!Char.IsDigit(number) && number != 8)
+            {
+                e.Handled = true;
+            }
+
+            
+        }
+    }
+}
